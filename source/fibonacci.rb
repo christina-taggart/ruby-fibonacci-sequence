ae40242f47efc5322a15f5024969f29cc2e648fb
Ricardo Rubio <rickarubio@gmail.com>
2014-01-08 11:44:17 -0800

    release 1: fibonacci terms can now be found recursively.

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -4,7 +4,10 @@ def fibonacci_iterative(n)
   fib[n]
 end
 
-def fibonacci_recursive(n)
+def fibonacci_recursive(n, fib=[0,1])
+  fib[fib.length] = fib[fib.length-1] + fib[fib.length-2]
+  return fib[n] if fib[n] != nil
+  fibonacci_recursive(n, fib)
 end
 
 # Driver Code
@@ -12,3 +15,8 @@ p fibonacci_iterative(5) == 5
 p fibonacci_iterative(3) == 2
 p fibonacci_iterative(0) == 0
 p fibonacci_iterative(1) == 1
+
+p fibonacci_recursive(5) == 5
+p fibonacci_recursive(3) == 2
+p fibonacci_recursive(0) == 0
+p fibonacci_recursive(1) == 1
