7040cbbcf46e87b2282d7d84e40746327f259aa4
Apprentice <apprentice@dbc08.local>
2014-01-08 11:54:20 -0800

    Release 0

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -1,5 +1,26 @@
 def fibonacci_iterative(n)
+  fib_seq = []
+  0.upto(n) do |i|
+    if i == 0
+      fib_seq[i] = 0
+    elsif i==1
+      fib_seq[i] = 1
+    else
+      fib_seq[i] = fib_seq[i-1] + fib_seq[i-2]
+    end
+  end
 end
 
+p fibonacci_iterative(0)
+
+
+
+  # fib_seq = [0,1]
+  # while i <= n
+  #   fib_seq << i
+  #   i += fib_seq[-2]
+  #   #[0,1,1,2,3,5]
+
 def fibonacci_recursive(n)
+
 end
