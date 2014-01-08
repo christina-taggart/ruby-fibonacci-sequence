162963b94a0b12eb6ca8b4441cbe2c384058febb
Apprentice <apprentice@dbc06.local>
2014-01-08 12:42:01 -0800

    releases 0 & 1: created iterative and recursive fib_finders

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -1,5 +1,30 @@
 def fibonacci_iterative(n)
+  first = 0
+  second = 1
+  n = n - 2
+  if n == 0
+    return first
+  else
+    while n > 0
+      fib = first + second
+      first = second
+      second = fib
+      n -= 1
+    end
+  end
+  return fib
 end
 
+#p fibonacci_iterative(7) == 8
+
 def fibonacci_recursive(n)
+  if n == 0
+    0
+  elsif n <= 1
+    1
+  else
+    fib = fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+  end
 end
+
+p fibonacci_recursive(7)
