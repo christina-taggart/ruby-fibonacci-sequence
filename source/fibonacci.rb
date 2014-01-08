f5b509c0a214ffd0a235ea16417f6ef45b207eeb
Ricardo Rubio <rickarubio@gmail.com>
2014-01-08 11:26:55 -0800

    release 0: iteratively finds the nth element of the fibonacci sequence

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -1,5 +1,14 @@
 def fibonacci_iterative(n)
+  fib = [0, 1]
+  fib[fib.length] = fib[fib.length-1] + fib[fib.length-2] until fib[n] != nil
+  fib[n]
 end
 
 def fibonacci_recursive(n)
 end
+
+# Driver Code
+p fibonacci_iterative(5) == 5
+p fibonacci_iterative(3) == 2
+p fibonacci_iterative(0) == 0
+p fibonacci_iterative(1) == 1
