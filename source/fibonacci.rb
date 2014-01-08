af2613f14027e366ffb244909c1902c263d3c560
Apprentice <apprentice@dbc05.local>
2014-01-08 14:57:37 -0800

    Release 0 and Release 1

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -1,5 +1,28 @@
-def fibonacci_iterative(n)
+def fibonacci_iterative(nth_fib)
+  first_num = 0
+  second_num = 1
+  next_num = 1
+  0.upto(nth_fib - 1) do
+    first_num = second_num
+    second_num = next_num
+    next_num = first_num + second_num
+  end
+  return first_num
 end
 
-def fibonacci_recursive(n)
+p fibonacci_iterative(6) == 8
+p fibonacci_iterative(8) == 21
+
+
+def fibonacci_recursive(nth_fib)
+  if nth_fib == 1 || nth_fib == 2
+    return 1
+  else
+    fibonacci_recursive(nth_fib - 1) + fibonacci_recursive(nth_fib - 2)
+  end
+
 end
+
+p fibonacci_recursive(3) == 2
+p fibonacci_recursive(9) == 34
+p fibonacci_recursive(10) == 55
