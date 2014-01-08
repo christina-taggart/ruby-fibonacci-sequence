7f1a43d7c475aeb6b8693816748818f03c4b21a5
Apprentice <apprentice@dbc01.local>
2014-01-08 15:13:42 -0800

    fib

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -1,5 +1,23 @@
 def fibonacci_iterative(n)
+  fib_old, fib_new = 0, 1
+  (n - 2).times do
+    fib_old, fib_new = fib_new, fib_old + fib_new
+  end
+  return fib_new
 end
 
 def fibonacci_recursive(n)
+  if n == 2 || n == 3
+    1
+  elsif n == 1
+    0
+  else
+    fibonacci_recursive(n - 2) + fibonacci_recursive(n - 1)
+  end
 end
+
+p fibonacci_iterative(3) #== 1
+p fibonacci_iterative(6) #== 5
+
+p fibonacci_recursive(9) == 21
+p fibonacci_recursive(1) == 0
