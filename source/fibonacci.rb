4ae20c6da2e4ffc46004d07f9082cedf7d0d9e89
Patrick <Patrick@Patricks-MacBook-Air.local>
2014-01-08 12:32:29 -0800

    Release 0: Implements iterative method to find nth Fibonacci

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -1,5 +1,24 @@
 def fibonacci_iterative(n)
+	return 0 if n == 0
+	return 1 if n == 1
+	fib_seq = [0,1]
+	(n-1).times do fib_seq << fib_seq[-2] + fib_seq[-1] end
+	fib_seq.last
 end
 
 def fibonacci_recursive(n)
 end
+
+p fibonacci_iterative(0) == 0
+p fibonacci_iterative(1) == 1
+p fibonacci_iterative(2) == 1
+p fibonacci_iterative(3) == 2
+p fibonacci_iterative(4) == 3
+p fibonacci_iterative(5) == 5
+
+p fibonacci_recursive(0) == 0
+p fibonacci_recursive(1) == 1
+p fibonacci_recursive(2) == 1
+p fibonacci_recursive(3) == 2
+p fibonacci_recursive(4) == 3
+p fibonacci_recursive(5) == 5
