fb5e10711cd04c26e13bd1a50f20e2780699fb1c
Apprentice <apprentice@dbc29.local>
2014-01-08 11:45:55 -0800

    Release 0: solve fibonacci iteratively

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -1,5 +1,24 @@
-def fibonacci_iterative(n)
+def fibonacci_iterative(number)
+  if number > 0
+    previous = 0
+    current = 1
+    (number - 1).times do
+    previous, current = current, current + previous
+    end
+    current
+  else
+    0
+  end
 end
 
 def fibonacci_recursive(n)
 end
+
+puts fibonacci_iterative 0
+puts fibonacci_iterative 1
+puts fibonacci_iterative 2
+puts fibonacci_iterative 3
+puts fibonacci_iterative 4
+puts fibonacci_iterative 5
+puts fibonacci_iterative 6
+puts fibonacci_iterative 7
