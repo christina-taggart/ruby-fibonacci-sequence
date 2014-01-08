82197cf9fd47db2903cc61b01a6057b710f3873b
Apprentice <apprentice@dbc29.local>
2014-01-08 11:51:27 -0800

    Release 1: solve fibonacci recursively

--- source/fibonacci.rb
+++ source/fibonacci.rb
@@ -11,9 +11,12 @@ def fibonacci_iterative(number)
   end
 end
 
-def fibonacci_recursive(n)
+def fibonacci_recursive(number, previous = 0, current = 1)
+  number > 0 ? fibonacci_recursive(number-1, current, current + previous) : current
 end
 
+
+puts "---- fibonacci_iterative ----"
 puts fibonacci_iterative 0
 puts fibonacci_iterative 1
 puts fibonacci_iterative 2
@@ -22,3 +25,13 @@ puts fibonacci_iterative 4
 puts fibonacci_iterative 5
 puts fibonacci_iterative 6
 puts fibonacci_iterative 7
+
+puts "---- fibonacci_recursive ----"
+puts fibonacci_recursive 0
+puts fibonacci_recursive 1
+puts fibonacci_recursive 2
+puts fibonacci_recursive 3
+puts fibonacci_recursive 4
+puts fibonacci_recursive 5
+puts fibonacci_recursive 6
+puts fibonacci_recursive 7
